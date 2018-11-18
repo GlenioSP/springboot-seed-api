6aff6ef74f7e0e0077a5b7b419cbc5f7593e3f98
gleniosp <gleniosp@algartech.com>
2018-11-18 16:42:51 -0200

    Remove response status from exceptions

--- src/main/java/com/example/exception/EntityConflictException.java
+++ src/main/java/com/example/exception/EntityConflictException.java
@@ -3,7 +3,6 @@ package com.example.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.CONFLICT)
 public class EntityConflictException extends BaseException {
 
     private static final long serialVersionUID = -255284434459554295L;
--- src/main/java/com/example/exception/EntityNotFoundException.java
+++ src/main/java/com/example/exception/EntityNotFoundException.java
@@ -3,10 +3,9 @@ package com.example.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends BaseException {
 
-    private static final long serialVersionUID = 6150554475676803638L;
+    private static final long serialVersionUID = 4065571577839079528L;
 
     public EntityNotFoundException(String msg) {
         super(msg);
